702052e4dd8e840d45e1b9b5586e86ff17359227
Danilo <mdminayo@gmail.com>
2020-03-03 15:31:39 -0500

    modificacion del mensaje

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -6,7 +6,7 @@ export const saludar = ( nombre ) => {
     console.log('creando h1, en el HTML');
 
     const h1 = document.createElement('h1');
-    h1.innerText = `Hola, ${ nombre }, vamos a esforzarnos por apreder ;)`;
+    h1.innerText = `Hola, ${ nombre }, estamos aprendiendo JS `;
 
     document.body.append(h1);
 }
